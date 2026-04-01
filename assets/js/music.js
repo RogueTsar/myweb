4d49b59292905372be49b0530b56b1071acf20c6
Vaishnavi Singh <144937169+TsarIvanTheTerrible@users.noreply.github.com>
2026-04-01 22:44:22 +0800

    Revert "Redesign/2026 04 01"

--- assets/js/music.js
+++ assets/js/music.js
@@ -263,32 +263,24 @@ function renderBarChart(containerId, items, mode) {
     var html = '<div class="bar-chart">';
     for (var j = 0; j < items.length; j++) {
         var item = items[j];
+        var label = mode === 'full' ? item.name + ' \u2014 ' + item.artist : item.name;
         var pct = Math.round(100 - ((item.rank - 1) / total) * 72);
-
-        // Build styled label: song name (maroon) + artist (highlighted) + genre tags
-        var labelHtml;
-        if (mode === 'full') {
-            labelHtml = '<span class="bar-chart-row__name">' + escapeHtml(item.name) + '</span> ' +
-                '<span class="bar-chart-row__artist-hl">' + escapeHtml(item.artist) + '</span>';
-        } else {
-            labelHtml = '<span class="bar-chart-row__name">' + escapeHtml(item.name) + '</span>';
-        }
-
-        // Genre tags
-        var genreHtml = '';
+        var tooltipText;
         if (item.genres && item.genres.length > 0) {
-            item.genres.forEach(function (g) {
-                genreHtml += ' <span class="bar-chart-row__genre-hl">' + escapeHtml(g) + '</span>';
-            });
+            tooltipText = item.genres.join(' \u00b7 ');
+        } else if (item.album) {
+            tooltipText = item.album;
+        } else {
+            tooltipText = '#' + item.rank;
         }
-
         html +=
             '<div class="bar-chart-row">' +
                 '<span class="bar-chart-row__rank">' + item.rank + '</span>' +
-                '<span class="bar-chart-row__label">' + labelHtml + genreHtml + '</span>' +
+                '<span class="bar-chart-row__label" title="' + escapeHtml(label) + '">' + escapeHtml(label) + '</span>' +
                 '<div class="bar-chart-row__bar-container">' +
                     '<div class="bar-chart-row__bar" data-width="' + pct + '%"></div>' +
                 '</div>' +
+                '<span class="bar-chart-row__tooltip">' + escapeHtml(tooltipText) + '</span>' +
             '</div>';
     }
     html += '</div>';
@@ -368,20 +360,20 @@ function renderPlaylists(playlists) {
         if (section) section.style.display = 'none';
         return;
     }
-    var html = '<div class="playlist-drawer">';
+    var html = '<div class="playlist-grid">';
     for (var i = 0; i < playlists.length; i++) {
         var p = playlists[i];
         var imgHtml = p.image
-            ? '<img class="playlist-drawer-card__img" src="' + escapeHtml(p.image) + '" alt="" loading="lazy">'
-            : '<div class="playlist-drawer-card__img playlist-drawer-card__img--placeholder"></div>';
+            ? '<img class="playlist-card__img" src="' + escapeHtml(p.image) + '" alt="" loading="lazy">'
+            : '<div class="playlist-card__img playlist-card__img--placeholder"></div>';
         var countText = p.track_count > 0 ? p.track_count + ' tracks' : '';
-        var descHtml = p.description ? '<div class="playlist-drawer-card__desc">' + escapeHtml(p.description) + '</div>' : '';
+        var descHtml = p.description ? '<div class="playlist-card__desc">' + escapeHtml(p.description) + '</div>' : '';
         html +=
-            '<a class="playlist-drawer-card" href="' + escapeHtml(p.url) + '" target="_blank" rel="noopener">' +
+            '<a class="playlist-card" href="' + escapeHtml(p.url) + '" target="_blank" rel="noopener">' +
                 imgHtml +
-                '<div class="playlist-drawer-card__body">' +
-                    '<div class="playlist-drawer-card__name">' + escapeHtml(p.name) + '</div>' +
-                    (countText ? '<div class="playlist-drawer-card__count">' + countText + '</div>' : '') +
+                '<div class="playlist-card__body">' +
+                    '<div class="playlist-card__name">' + escapeHtml(p.name) + '</div>' +
+                    (countText ? '<div class="playlist-card__count">' + countText + '</div>' : '') +
                     descHtml +
                 '</div>' +
             '</a>';
